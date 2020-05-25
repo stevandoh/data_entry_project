1d9faf7640c3419805bd42b53b6fa6f5bf134fc4
Steve Dawsonn-Andoh <stevandoh@gmail.com>
2020-05-25 17:00:53 +0000

    initial commit

---
+++ dn.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+def getfiles():
+    return [os.path.join(root, file) for root, dirs, files in os.walk(os.path.abspath(os.getcwd())) for file in files if
+            file.endswith(".pdf".lower())]
+
+print(getfiles())
